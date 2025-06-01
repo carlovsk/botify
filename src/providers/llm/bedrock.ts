5416e290571d417202b87f11594b231fc475b1cb
Carlos Daniel <carlosdnbarboza@gmail.com>
2025-06-01 12:01:45 -0300

    feat: use claude 3 haiku

--- src/providers/llm/bedrock.ts
+++ src/providers/llm/bedrock.ts
@@ -1,10 +1,11 @@
 import { ChatBedrockConverse } from '@langchain/aws';
 import { z } from 'zod';
 
-enum Models {
+enum Model {
   Claude35Haiku = 'us.anthropic.claude-3-5-haiku-20241022-v1:0',
   Claude3Haiku = 'us.anthropic.claude-3-haiku-20240307-v1:0',
   Llama4Scout17B = 'us.meta.llama4-scout-17b-instruct-v1:0',
+  NovaLite = 'amazon.nova-lite-v1:0',
 }
 
 export class LlmProvider {
@@ -12,7 +13,7 @@ export class LlmProvider {
 
   constructor() {
     this.client = new ChatBedrockConverse({
-      model: Models.Claude35Haiku,
+      model: Model.Claude3Haiku,
       region: z.string().parse(process.env.AWS_REGION),
       temperature: 0.3,
     });
